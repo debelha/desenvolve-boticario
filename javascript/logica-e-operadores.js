d9a400711bb55797baf893d011573e6b9483bd88
debelha <dleitedias@gmail.com>
2025-12-16 19:27:26 -0300

    files refactor

--- javascript/logica-e-operadores.js
+++
@@ -1,9 +0,0 @@
-const referenciaBancaria = {
-    banco: '001',
-    agencia: '1234',
-    conta: '56789-0',
-    digito: '1'
-    };
-
-    console.log(referenciaBancaria.agencia)
-
--- mentoria/exercicio1.js
+++
@@ -1,5 +0,0 @@
-let numero1 = 5;
-let numero2 = 10;
-
-soma = numero1 + numero2;
-console.log("A soma é: " + soma);
