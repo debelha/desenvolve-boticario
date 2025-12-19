1dde9c20e4c315b4f329ad69ee7d53460e1ffee3
debelha <dleitedias@gmail.com>
2025-12-19 20:19:33 -0300

    add arrays and objects sambple

--- 2-node/file.js
+++ 2-node/file.js
@@ -1,7 +1,16 @@
-let name = "Deborah";
-let age = 27;
-let college = true;
-const age2 = 31;
+//exemplo array
+const phoneList = ["11954565456", "11923314563", "1197795456"];
 
-name = "João";
-console.log(name, age, college);
+sms.send(phoneList[0], "Olá, tudo bem?");
+sms.send(phoneList[1], "Olá, tudo bem?");
+sms.send(phoneList[2], "Olá, tudo bem?");
+
+//exemplo objeto 
+const referenciaBancaria = {
+    banco: "Banco do Brasil",
+    agencia: "1234",
+    conta: "789-4",
+    telefone: "12354398796"
+};
+
+console.log(referenciaBancaria.banco);
