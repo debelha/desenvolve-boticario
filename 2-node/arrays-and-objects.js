2723a358c98955ee203ed428d5f21b0863cf33a0
debelha <dleitedias@gmail.com>
2025-12-29 17:49:19 -0300

    add arrays and objects sample

--- 2-node/arrays-and-objects.js
+++ 2-node/arrays-and-objects.js
@@ -1,11 +1,11 @@
-//exemplo array
+//array
 const phoneList = ["11954565456", "11923314563", "1197795456"];
 
 sms.send(phoneList[0], "Olá, tudo bem?");
 sms.send(phoneList[1], "Olá, tudo bem?");
 sms.send(phoneList[2], "Olá, tudo bem?");
 
-//exemplo objeto 
+//object
 const referenciaBancaria = {
     banco: "Banco do Brasil",
     agencia: "1234",
