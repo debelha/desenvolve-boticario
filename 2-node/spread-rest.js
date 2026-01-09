f4c3b353e7861a18647d3d67b30a56bb293fc1ec
debelha <dleitedias@gmail.com>
2026-01-08 21:15:19 -0300

    add spread and rest sample

--- 2-node/spread-rest.js
+++ 2-node/spread-rest.js
@@ -1,24 +1,44 @@
-//1 normal function 
+//normal function 
 function welcomeUser(nome) {
     console.log(`${nome}, welcome to the class!`);
 }
-
-//2 arrow function 
+//arrow function 
 const welcomeUser2 = (nome) => {
     console.log(`${nome}, welcome to the class!`);
 }
-
+//execution 
 welcomeUser("Deborah"); 
 welcomeUser2("Matheus"); 
 
 
-//1 arrow function 
+//arrow function 
 const calculateIMC = (peso, altura) => {
     return peso / (altura * altura);
 }
-
-//2 simple arrow function  
+//simple arrow function  
 const calculateIMC2 = (peso, altura) => peso / (altura * altura);
-
 //execution 
 console.log(calculateIMC(80, 1.8));
+
+
+//rest
+function soma(...parcelas) {
+    return parcelas.reduce((total, valor) => {
+        return total + valor;
+    }, 0); 
+}
+//execution 
+console.log(soma(1));
+console.log(soma(1, 2));
+console.log(soma(1, 2, 3));
+console.log(soma(1, 2, 3, 4));
+console.log(soma(1, 2, 3, 4, 5));
+console.log(soma(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+
+//spread 
+const numeros1 = [1, 2, 3, 4, 5];
+const numeros2 = [6, 7, 8, 9, 10];
+const novoNumero = [...numeros1, ...numeros2];
+
+console.log(novoNumero);
